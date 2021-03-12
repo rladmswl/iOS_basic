bd979a0e6cc2a3e43a4edd5eeadb34c081566ba3
Kim eunji <emliy112@naver.com>
2021-03-12 14:17:11 +0900

    타입

--- DateType.playground/Contents.swift
+++ DateType.playground/Contents.swift
@@ -31,3 +31,26 @@ var someString: String = "하하"
 someString = someString + " 스마일~"
 
 
+
+// Any : Swift의 모든 타입을 지칭하는 키워드
+var someAny: Any = 100
+someAny = "어떤 타입도 수용 가능"
+someAny = 123.12
+
+// let someDouble: Couble = someAny // 오류 발생. 왜???
+
+
+
+// AnyObject : 모든 클래스 타입을 지칭하는 프로토콜
+class SomeClass {}
+
+var someAnyObject: AnyObject = SomeClass() // 클래스의 인스턴스 SomeClass()를 변수 someAnyObject에 넣기
+
+//someAnyObject = 123.12 // 오류 발생. 클래스 타입이 아니라서.
+
+
+
+// nil : '없을'을 나타내는 키워드 > nil이 필요한 부분은 옵셔너리에서!
+// someAny = nil // 어떤 타입도 다 넣을 수 있지만, 빈 값은 넣을 수 없다.
+// somenyObject = nil // 여기도 마찬가지
+
